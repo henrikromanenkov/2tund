e9452dddcfbb38242a1985a0c9359fb417483778
Henrik Romanenkov <henrrom@greeny.cs.tlu.ee>
2015-09-09 12:38:31 +0300

    Added Login page

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+
+	
+	
+	
+
+?>
+<html>
+<head>
+	<title>Login page</title>
+	
+</head>
+<body>
+	<h2>Log in</h2>
+	
+		<input type="email" placeholder=0"E-post"> <br><br>
+		<input type="password" placeholder="Parool"> <br><br>
+		<input type="submit" value="Log in"> 
+		
+		
+		
+	<h2>Create useer</h2>
+</body>
+
+</html>
