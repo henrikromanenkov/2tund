f3e54f045e3030ad0bb19c11af988169759664a6
Henrik Romanenkov <henrrom@greeny.cs.tlu.ee>
2015-09-09 13:21:44 +0300

    Password error

--- login.php
+++ login.php
@@ -1,9 +1,31 @@
 <?php
  
+	//echo$_POST["email"];
+	//echo$_POST["password"];
 	
-
-	echo$_GET["email"];
-
+	$email_error = "";
+	$password_error = "";
+	//kontrollime, et keegi vajutas input nuppu.
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		//echo "Keegi vajutas nuppu";
+		
+		//kontrollin, et e-post ei ole tühi
+		if(empty($_POST[$email]) ){
+			$email_error = " See väli on kohustuslik."
+		}
+		
+		//kontrollin, et parool ei ole tühi
+		if(empty($_POST[$password]) ){
+			$password_error = "See väli on kohustuslik."
+		}else {	
+			
+			// kui oleme siia jõudnud, siis parool ei ole tühi
+			//kontrollin, et oleks vähemalt 8 sümbolit pikk
+			strlen
+			if(strlen($_POST[$password]) < 8) {
+				$password_error = "Peab olema vähemalt 8 tähemärki pikk"
+		}
+	}
 ?>
 <html>
 <head>
@@ -12,9 +34,9 @@
 </head>
 <body>
 	<h2>Log in</h2>
-		<form action="Login.php" method="get">
-			<input name="email" type="email" placeholder="E-post"> <br><br>
-			<input name="password" type="password" placeholder="Parool"> <br><br>
+		<form action="Login.php" method="post">
+			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
+			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
 			<input type="submit" value="Log in"> 
 		</form>
 		
